86c9595dec2dad9b6020540645eb1bae0c62c387
Miguel París Díaz <mparisdiaz@gmail.com>
2012-03-01 14:49:09 +0100

    Move utils.h and utils.c to util directory.

--- media/util/utils.c
+++ media/util/utils.c
@@ -1,7 +1,7 @@
 
-#include <utils.h>
+#include "utils.h"
 
-int64_t
+int
 timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p)
 {
 	return ( ((timeA_p->tv_sec * 1000000000) + timeA_p->tv_nsec) -
--- media/util/utils.h
+++ media/util/utils.h
@@ -3,6 +3,6 @@
 
 #include <time.h>
 
-int64_t timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p);
+int timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p);
 
 #endif /* __UTILS_H__ */
