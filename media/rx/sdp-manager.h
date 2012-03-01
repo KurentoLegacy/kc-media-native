18ed95a5c70fc8aa1c9eee65c98c8deba73069f4
Miguel París Díaz <mparisdiaz@gmail.com>
2012-03-01 16:27:39 +0100

    Ensure only include sdp-manager.h once.

--- media/rx/sdp-manager.h
+++ media/rx/sdp-manager.h
@@ -1,11 +1,9 @@
-/**
- * 
- * @author Miguel París Díaz
- * 
- */
+#ifndef __SDP_MANAGER_H__
+#define __SDP_MANAGER_H__
  
 #include "libavformat/avformat.h"
 
+int av_open_input_sdp(AVFormatContext **ic_ptr, const char *sdp_str,
+								AVFormatParameters *ap);
 
-int av_open_input_sdp(AVFormatContext **ic_ptr, const char *sdp_str, AVFormatParameters *ap);
-
+#endif /* __SDP_MANAGER_H__ */
