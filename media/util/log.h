7dda352ba152bd1e94f79952fdf24f7681bf19cf
Miguel París Díaz <mparisdiaz@gmail.com>
2012-03-01 14:52:16 +0100

    Add basic log functionality.

---
+++ media/util/log.h
@@ -0,0 +1,23 @@
+#ifndef __LOG_H__
+#define __LOG_H__
+
+#include <stdarg.h>
+
+#define LOG_BUF_SIZE 256
+
+typedef enum _MediaLogLevel {
+	MEDIA_LOG_UNKNOWN = 0,
+	MEDIA_LOG_VERBOSE,
+	MEDIA_LOG_DEBUG,
+	MEDIA_LOG_INFO,
+	MEDIA_LOG_WARN,
+	MEDIA_LOG_ERROR,
+	MEDIA_LOG_FATAL,
+} MediaLogLevel;
+
+void media_log(int level, const char *tag, const char *fmt, ...);
+void media_vlog(int level, const char *tag, const char *fmt, va_list vl);
+void media_log_set_callback(void (*callback)(int level, const char *tag,
+					     const char *fmt, va_list vargs));
+
+#endif /* __LOG_H__ */
