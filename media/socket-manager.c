0e512f1dd1cc1312a1f032f7b677f419d6b7f320
Miguel París Díaz <mparisdiaz@gmail.com>
2012-03-26 09:54:15 +0200

    Close correctly AVFormatContext in socket-manager.c
    Now we close the AVIOContext associated to the AVFormatContext
    with avio_close function.
    With this we fix the problem to left the UDP ports opened.

--- media/socket-manager.c
+++ media/socket-manager.c
@@ -128,14 +128,16 @@ static void free_connection(URLContext *urlContext) {
 	if (pAudioFormatCtx && pAudioFormatCtx->pb && (urlContext
 			== pAudioFormatCtx->pb->opaque) && (--nAudio == 0)) {
 		media_log(MEDIA_LOG_DEBUG, LOG_TAG, "free pAudioFormatCtx");
-		av_free(pAudioFormatCtx);
+		avio_close(pAudioFormatCtx->pb);
+		avformat_free_context(pAudioFormatCtx);
 		media_log(MEDIA_LOG_DEBUG, LOG_TAG, "free ok");
 		pAudioFormatCtx = NULL;
 	}
 	if (pVideoFormatCtx && pVideoFormatCtx->pb && (urlContext
 			== pVideoFormatCtx->pb->opaque) && (--nVideo == 0)) {
 		media_log(MEDIA_LOG_DEBUG, LOG_TAG, "free pVideoFormatCtx");
-		av_free(pVideoFormatCtx);
+		avio_close(pVideoFormatCtx->pb);
+		avformat_free_context(pVideoFormatCtx);
 		media_log(MEDIA_LOG_DEBUG, LOG_TAG, "free ok");
 		pVideoFormatCtx = NULL;
 	}
