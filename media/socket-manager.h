d3a9caf6308f3d0c5df71a8018ec95219674e9b4
Miguel París Díaz <mparisdiaz@gmail.com>
2012-03-29 10:51:07 +0200

    Add take and release local port functions definitions in socket-manager.h

--- media/socket-manager.h
+++ media/socket-manager.h
@@ -9,4 +9,9 @@ URLContext* get_connection_by_local_port(int local_port, enum AVMediaType media_
 URLContext* get_audio_connection(int audioPort);
 URLContext* get_video_connection(int videoPort);
 
+int take_audio_local_port(int audioPort);
+int release_audio_local_port();
+int take_video_local_port(int videoPort);
+int release_video_local_port();
+
 #endif /* __SOCKET_MANAGER_H__ */
