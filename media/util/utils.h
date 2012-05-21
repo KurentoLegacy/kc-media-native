59435004b84b430196ce45eec512a7cc5c87ca65
Miguel París Díaz <mparisdiaz@gmail.com>
2012-05-21 14:40:08 +0200

    Remove timespecDiff function. It is unnecessary.

--- media/util/utils.h
+++ media/util/utils.h
@@ -1,11 +1,9 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
 
-#include <time.h>
 #include <stdint.h>
 #include "libavutil/rational.h"
 
-int64_t timespecDiff(struct timespec *timeA_p, struct timespec *timeB_p);
 int64_t get_pts(int64_t time, AVRational clock_rate);
 
 #endif /* __UTILS_H__ */
