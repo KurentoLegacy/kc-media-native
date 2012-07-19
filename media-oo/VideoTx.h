751de08c54ac40ca63ca6bb767da16e4c9ce22d4
Miguel París Díaz <mparisdiaz@gmail.com>
2012-07-19 18:32:14 +0200

    Add VideoTx class.
    This is a first approximation.

---
+++ media-oo/VideoTx.h
@@ -0,0 +1,40 @@
+
+#ifndef VIDEO_TX
+#define VIDEO_TX
+
+extern "C" {
+#include <stdint.h>
+
+#include "libavformat/avformat.h"
+#include <libavutil/pixfmt.h>
+}
+
+namespace media {
+	class VideoTx {
+	private:
+		AVOutputFormat *_fmt;
+		AVFormatContext *_oc;
+		AVStream *_video_st;
+
+		AVFrame *_picture, *_tmp_picture;
+		uint8_t *_picture_buf;
+		uint8_t *_video_outbuf;
+		int _video_outbuf_size, _n_frame;
+		enum PixelFormat _src_pix_fmt;
+
+	public:
+		VideoTx(const char* outfile, int width, int height,
+			int frame_rate_num, int frame_rate_den,
+			int bit_rate, int gop_size, enum CodecID codecId,
+			int payload_type, enum PixelFormat src_pix_fmt);
+		~VideoTx();
+		int putVideoFrameTx(uint8_t* frame, int width, int height, int64_t time);
+	private:
+		AVStream* addVideoStream(enum CodecID codec_id,
+					int width, int height, int frame_rate_num,
+					int frame_rate_den, int bit_rate, int gop_size);
+		int openVideo();
+	};
+}
+
+#endif /* VIDEO_TX */
